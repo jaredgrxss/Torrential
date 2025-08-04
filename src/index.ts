ea23894e9ea0ccde26ab04f6db26a35910582a63
Jared Gross <jaredgrossthe@gmail.com>
2025-08-04 08:26:11 -0500

    decoding first torrent file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+import bencode from "bencode";
+import { fileURLToPath } from "url";
 
+try {
+  const fileName = fileURLToPath(import.meta.url);
+  const dir = path.dirname(fileName);
+  const filePath = path.join(dir, "big-buck-bunny.torrent");
 
-function run(x: number): number {
-    return x * 2
-};
+  const torrent: any = bencode.decode(fs.readFileSync(filePath));
+  console.log(torrent.announce.toString("utf8"));
+} catch (e) {
+  console.log(`Error fetching file ${e}`);
+}
