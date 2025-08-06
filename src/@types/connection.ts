6714fcc37500fd861443cf970ff3d896939d7289
Jared Gross <jaredgrossthe@gmail.com>
2025-08-06 08:27:19 -0500

    working on parsing main torrent request

--- src/@types/connection.ts
+++ src/@types/connection.ts
@@ -1,5 +1,16 @@
 export interface ConnectionResponse {
-  action: number,
-  transactionId: number,
-  connectionId: Buffer<ArrayBufferLike> 
-} 
+  action: number;
+  transactionId: number;
+  connectionId: Buffer<ArrayBufferLike>;
+}
+
+export interface AnnounceResponse {
+  action: number;
+  transactionId: number;
+  leechers: number;
+  seeders: number;
+  peers: {
+    ip: string;
+    port: number;
+  }[];
+}
